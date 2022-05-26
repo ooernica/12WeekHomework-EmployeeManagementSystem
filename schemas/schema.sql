2f4c72ff5a894f8e267223eb57274bd5b540532b
Erna Lukac <e.lukac4@gmail.com>
2022-05-25 18:32:24 -0600

    database in sql

---
+++ schemas/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS employerTracker;
+CREATE DATABASE employerTracker;
+USE employerTracker;
+
+CREATE table department)
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL, 
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department (id)
+);
+
+CREATE TABLE employee(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT DEFAULT NULL,
+    FOREIGN KEY (role_id) REFERENCES role (id),
+    FOREIGN KEY (manager_id) REFERNCES employee (id)
+);
+
+
+

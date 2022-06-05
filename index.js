e988424404b321fc789af50eb5f7becd8dca99f0
Erna Lukac <e.lukac4@gmail.com>
2022-06-05 14:30:43 -0600

    able to add new role and new dept

--- index.js
+++ index.js
@@ -16,7 +16,16 @@ const everything = () => {
         {
             type: 'list',
             message: 'Choose your destiny',
-            choices: ['department', 'role', 'employee'],
+            choices: [
+                'department', 
+                'role', 
+                'employee',
+                'ADD department', 
+                'ADD role',
+                'ADD employee',
+                'UPDATE employee role',  
+            ],
+
             name: 'destinies',
         }
     ])
@@ -28,6 +37,14 @@ const everything = () => {
             viewRole();
         } else if (answers.destinies==='employee') {
             viewEmployee();
+        } else if (answers.destinies==='ADD department') {
+            addDept();
+        } else if (answers.destinies==='ADD role') {
+            addRole();
+        } else if (answers.destinies==='ADD employee') {
+            addEmployee();
+        } else if (answers.destinies==='UPDATE employee role') {
+            updateEmployeeRole();
         }
     })
 };
@@ -61,5 +78,107 @@ const viewEmployee = () => {
         everything();
     })
 };
+
+const addDept = () => {
+    console.log('something!!')
+    inquirer.prompt([{ 
+        type: 'input',
+        message: 'Enter name of new department',
+        name: 'newDept',
+    }])
+    .then(answers => {
+        const newDept=answers.newDept
+        db.query('INSERT INTO department (name) VALUES (?);', [newDept], (err, result) => {
+            if (err) {
+                console.log(err)
+            } 
+            everything();
+        })
+    })
+};
+
+const addRole = () => {
+    const listOptions = [];
+    const deptOptions = [];
+    db.query('SELECT * FROM department;', (err, result) =>{
+        if (err) {
+            console.log(err)
+        } 
+        result.forEach (i => {
+            listOptions.push(i.name);
+            deptOptions.push([i.id,i.name])
+        } )
+    })
+    inquirer.prompt([
+        { 
+            type: 'input',
+            message: 'Eneter name of new role',
+            name: 'newRole',
+        },
+        {
+            type: 'input',
+            message: "Enter salary of new role",
+            name: "newSalary",
+        },
+        {
+            type: 'list',
+            message: "Which department is this role a part of?",
+            choices: listOptions,
+            name: 'chooseDept',
+        }
+])
+    .then(answers => {
+        const newRole=answers.newRole;
+        const newSalary=answers.newSalary;
+        const chooseDept=answers.chooseDept;
+        let newDeptNo;
+        deptOptions.forEach(dept => {
+            if(chooseDept===dept[1]) {
+                newDeptNo=dept[0];
+            }
+        })
+        db.query('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?);', [newRole, newSalary, newDeptNo], (err, result) => {
+            if (err) {
+                console.log(err)
+            } 
+            everything();
+        })
+    })
+};
+
+const addEmployee = () => {
+    // inquirer.prompt([{ 
+    //     type: 'input',
+    //     message: 'Eneter name of new department',
+    //     name: 'newDept',
+    // }])
+    // .then(answers => {
+    //     const newDept=answers.newDept
+    //     db.query('INSERT INTO department (name) VALUES (?);', [newDept], (err, result) => {
+    //         if (err) {
+    //             console.log(err)
+    //         } 
+    //         everything();
+    //     })
+    // })
+};
+
+const updateEmployeeRole = () => {
+    // inquirer.prompt([{ 
+    //     type: 'input',
+    //     message: 'Eneter name of new department',
+    //     name: 'newDept',
+    // }])
+    // .then(answers => {
+    //     const newDept=answers.newDept
+    //     db.query('INSERT INTO department (name) VALUES (?);', [newDept], (err, result) => {
+    //         if (err) {
+    //             console.log(err)
+    //         } 
+    //         everything();
+    //     })
+    // })
+};
+
 
 everything();

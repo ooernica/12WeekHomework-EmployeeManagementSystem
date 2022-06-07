81ce5e742ff7878cedccb856a1d2294dc1f67a72
Erna Lukac <e.lukac4@gmail.com>
2022-06-06 18:09:06 -0600

    readme

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ const viewDept = () => {
 };
 
 const viewRole = () => {
-    db.query('SELECT * FROM role;', (err, result)=> {
+    db.query('SELECT R.id, R.title, R.salary, D.name department FROM role R INNER JOIN department D ON R.department_id=D.id;', (err, result)=> {
         if (err) {
             console.log(err)
         } 
@@ -70,7 +70,7 @@ const viewRole = () => {
 };
 
 const viewEmployee = () => {
-    db.query('SELECT * FROM employee;', (err, result)=> {
+    db.query("SELECT E.id, E.first_name, E.last_name, R.title, D.name department, R.salary, CONCAT(M.first_name, ' ', M.last_name) manager FROM employee E INNER JOIN role R ON E.role_id = R.id INNER JOIN department D ON R.department_id = D.id LEFT JOIN employee M ON E.manager_id = M.id;", (err, result)=> {
         if (err) {
             console.log(err)
         } 
@@ -145,39 +145,148 @@ const addRole = () => {
         })
     })
 };
+const listRoles = [];
+    const roleOption = [];
+    const listManagers = [];
+    const managerOption = [];
 
 const addEmployee = () => {
-    // inquirer.prompt([{ 
-    //     type: 'input',
-    //     message: 'Eneter name of new department',
-    //     name: 'newDept',
-    // }])
-    // .then(answers => {
-    //     const newDept=answers.newDept
-    //     db.query('INSERT INTO department (name) VALUES (?);', [newDept], (err, result) => {
-    //         if (err) {
-    //             console.log(err)
-    //         } 
-    //         everything();
-    //     })
-    // })
+    db.query('SELECT * FROM role;', (err, result) =>{
+        if (err) {
+            console.log(err)
+        } 
+        result.forEach (i => {
+            listRoles.push(i.title);
+            roleOption.push([i.id,i.title])
+        } )
+    });
+    db.query('SELECT * FROM employee;', (err, result) =>{
+        if (err) {
+            console.log(err)
+        } 
+        result.forEach (i => {
+            listManagers.push(i.first_name + ' ' + i.last_name);
+            managerOption.push([i.id,i.first_name + ' ' + i.last_name])
+        } )
+    });
+    inquirer.prompt([
+        { 
+            type: 'input',
+            message: 'Enter first name of new employee',
+            name: 'newFirstName',
+        },
+        {
+            type: 'input',
+            message: 'Enter last name of new employee',
+            name: "newLastName",
+        },
+        {
+            type: 'list',
+            message: 'what is the role?',
+            choices: listRoles,
+            name: 'newEmpRole',
+        },
+        {
+            type: 'list',
+            message: 'Who manages this employee?',
+            choices: listManagers,
+            name: 'employeesManager',
+        }
+])
+    .then(answers => {
+        const newFirstName=answers.newFirstName;
+        const newLastName=answers.newLastName;
+        const newEmpRole=answers.newEmpRole;
+        const employeesManager=answers.employeesManager;
+        console.log(newEmpRole);
+        let newRoleNo;
+        roleOption.forEach(rol => {
+            if(newEmpRole===rol[1]) {
+                newRoleNo=rol[0];
+            }
+        })
+        let newEmpNo;
+        managerOption.forEach(employee => {
+            if(employeesManager===employee[1]) {
+                newEmpNo=employee[0];
+            }
+        })
+        db.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?);', [newFirstName, newLastName, newRoleNo, newEmpNo], (err, result) => {
+            if (err) {
+                console.log(err)
+            } 
+            everything();
+        })
+    })
 };
 
+const listEmp = [];
+const empOptions = [];
+const allRoles = [];
+const roleOptions = [];
+
 const updateEmployeeRole = () => {
-    // inquirer.prompt([{ 
-    //     type: 'input',
-    //     message: 'Eneter name of new department',
-    //     name: 'newDept',
-    // }])
-    // .then(answers => {
-    //     const newDept=answers.newDept
-    //     db.query('INSERT INTO department (name) VALUES (?);', [newDept], (err, result) => {
-    //         if (err) {
-    //             console.log(err)
-    //         } 
-    //         everything();
-    //     })
-    // })
+    db.query('SELECT * FROM role;', (err, result) =>{
+        if (err) {
+            console.log(err)
+        } 
+        result.forEach (i => {
+            allRoles.push(i.title);
+            roleOptions.push([i.id,i.title])
+        } )
+    });
+    db.query('SELECT * FROM employee;', (err, result) =>{
+        if (err) {
+            console.log(err)
+        } 
+        result.forEach (i => {
+            listEmp.push(i.first_name + ' ' + i.last_name);
+            empOptions.push([i.id,i.first_name + ' ' + i.last_name])
+        } )
+    });
+    inquirer.prompt([
+        {   
+            type: 'input',
+            message: 'hit Enter',
+            name: "dummy"
+
+        },
+        { 
+            type: 'list',
+            message: 'Choose employee to update',
+            choices: listEmp,
+            name: 'editEmp',
+        },
+        {
+            type: 'list',
+            message: "Choose new role",
+            choices: allRoles,
+            name: "updatedRole",
+        }
+
+])
+    .then(answers => {
+        const editEmp=answers.editEmp;
+        const updatedRole=answers.updatedRole;
+        let newEmpNo;
+        empOptions.forEach(employee => {
+            if(editEmp===employee[1]) {
+                newEmpNo=employee[0];
+            }
+        })
+        let newRoleNo;
+        roleOptions.forEach(role => {
+            if(updatedRole===role[1]) {
+                newRoleNo=role[0];
+            }
+        })
+        db.query('UPDATE employee SET role_id=? WHERE id=?;', [newRoleNo, newEmpNo], (err, result) => {
+            if (err) {
+                console.log(err)
+            } 
+            everything();
+        })
+    })
 };
 
 
